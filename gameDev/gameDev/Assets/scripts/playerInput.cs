a4f4b4ae2a2ae9579425e658174ce4504057608f
a100531 <daniel.cassar.a100531@mcast.edu.mt>
2017-12-13 14:16:21 +0100

    character controller edit
    character can now jump on slopes
    and also character switching works
    so it stops earth from jumping
    (very dirty)

--- gameDev/gameDev/Assets/scripts/playerInput.cs
+++ gameDev/gameDev/Assets/scripts/playerInput.cs
@@ -11,27 +11,33 @@ public class playerInput : MonoBehaviour {
 	public float moveSpeed;
 	private float fallSpeed;
 
+	public bool jumpAir;
+
 	// Use this for initialization
 	void Start () {
 		characterController = GetComponent<CharacterController>();
 		gameObject.GetComponent<Renderer>().material.color = Color.blue;
+		jumpAir = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		IsGrounded();
-		Fall();
-		Jump();
-		Move();	
+		
 
 		if (Input.GetKey(KeyCode.Alpha1))
         {
             gameObject.GetComponent<Renderer>().material.color = Color.blue;
+			jumpAir = true;
         }
         if (Input.GetKey(KeyCode.Alpha2))
         {
             gameObject.GetComponent<Renderer>().material.color = Color.green;
+			jumpAir = false;
         }
+		IsGrounded();
+		Fall();
+		Jump();
+		Move();	
 	
 	}
 	void Move(){
@@ -39,7 +45,7 @@ public class playerInput : MonoBehaviour {
 		if(xSpeed != 0) characterController.Move(new Vector3(xSpeed, 0) * moveSpeed * Time.deltaTime);
 	}
 	void Jump(){
-		if(Input.GetButtonDown ("Jump") && isGrounded){
+		if(Input.GetButtonDown ("Jump") && isGrounded && jumpAir){
 			fallSpeed = -jumpSpeed;
 		}
 	}
@@ -54,6 +60,17 @@ public class playerInput : MonoBehaviour {
 	}
 	void IsGrounded(){
 		isGrounded = (Physics.Raycast(transform.position, -transform.up, characterController.height/1.8f));
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "box"){
+			Debug.Log("interact");
+		}
+		if(other.tag == "slope"){
+			Debug.Log("slope");
+			isGrounded = true;
+		}
 	}
 
 }
