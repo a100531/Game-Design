155272561b7d669203271505823e8570eab004f7
Daniel <cassardan0@gmail.com>
2017-12-13 23:10:46 +0100

    created prototype
    1 level created
    player is able to turn on and off objects
    player dies and reloads level
    first puzzles created

--- gameDev/gameDev/Assets/scripts/playerInput.cs
+++ gameDev/gameDev/Assets/scripts/playerInput.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerInput : MonoBehaviour {
 
@@ -11,8 +12,19 @@ public class playerInput : MonoBehaviour {
 	public float moveSpeed;
 	private float fallSpeed;
 	public GameObject platform1;
+	public GameObject platform2;
+	public GameObject platform3;
+	public GameObject platform4;
+	public GameObject platform5;
+	public GameObject platform6;
+	public GameObject platform7;
+	public GameObject platform8;
+	public GameObject platform9;
+	public GameObject switch5;
+	public GameObject switch8;
 
 	public bool jumpAir;
+	public bool isEarth = false;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +32,12 @@ public class playerInput : MonoBehaviour {
 		gameObject.GetComponent<Renderer>().material.color = Color.blue;
 		jumpAir = true;
 		platform1.SetActive(false);
+		platform2.SetActive(false);
+		platform3.SetActive(false);
+		platform4.SetActive(false);
+		platform7.SetActive(false);
+		platform8.SetActive(false);
+		switch8.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -30,11 +48,13 @@ public class playerInput : MonoBehaviour {
         {
             gameObject.GetComponent<Renderer>().material.color = Color.blue;
 			jumpAir = true;
+			isEarth = false;
         }
         if (Input.GetKey(KeyCode.Alpha2))
         {
             gameObject.GetComponent<Renderer>().material.color = Color.green;
 			jumpAir = false;
+			isEarth = true;
         }
 		IsGrounded();
 		Fall();
@@ -66,13 +86,69 @@ public class playerInput : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.tag == "box"){
+		if(other.tag == "box" && isEarth){
 			Debug.Log("interact");
 			platform1.SetActive(true);
+		}
+		if(other.tag == "box1" && isEarth){
+			Debug.Log("interact");
+			platform2.SetActive(true);
+			
+		}
+		if(other.tag == "box2" && isEarth){
+			Debug.Log("interact");
+			platform3.SetActive(true);
+			
+		}
+		if(other.tag == "box3" && isEarth){
+			Debug.Log("interact");
+			platform4.SetActive(true);
+			platform9.SetActive(false);
+			
+			
+		}
+		if(other.tag == "box4" && isEarth){
+			Debug.Log("interact");
+			platform5.SetActive(false);
+			
+		}
+		if(other.tag == "box5" && isEarth){
+			Debug.Log("interact");
+			platform6.SetActive(false);
+			platform7.SetActive(true);
+			platform8.SetActive(true);
+			switch8.SetActive(false);
+	
+		}
+		if(other.tag == "box6" && isEarth){
+			Debug.Log("interact");
+			platform6.SetActive(true);
+			platform7.SetActive(false);
+	
+		}
+		if(other.tag == "box7" && isEarth){
+			Debug.Log("interact");
+			platform6.SetActive(false);
+			switch8.SetActive(true);
+	
+		}
+		if(other.tag == "box8" && isEarth){
+			Debug.Log("interact");
+			platform8.SetActive(false);
+			switch5.SetActive(false);
+			
+	
 		}
 		if(other.tag == "slope"){
 			Debug.Log("slope");
 			isGrounded = true;
+		}
+		
+	}
+	void OnTriggerStay(Collider other)
+	{
+		if(other.tag == "deathwall"){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 		}
 	}
 
