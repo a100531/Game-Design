73f2559b2a39ecc57cabb7a042bc5d89ea608b78
Daniel <daniel.cassar.a100531@mcast.edu.mt>
2018-01-21 23:38:12 +0100

    created the fuckyoustone

---
+++ gameDev/gameDev/Assets/scripts/StoneMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneMove : MonoBehaviour {
+
+	public float move = 4;
+	public float speed = 10;
+	private float initialPositionL ;
+	
+	public bool right = false;
+	
+	public GameObject Stone;
+	
+
+	
+	
+	private Player player;
+	// Use this for initialization
+	void Start () {
+		
+		initialPositionL = Stone.transform.position.x;
+		Debug.Log(initialPositionL);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(right)
+		{
+			Stone.transform.Translate(speed*Time.deltaTime,0,0);
+			//Stone.transform.Rotate(0,0,speed*Time.deltaTime);
+			if(Stone.transform.position.x >= initialPositionL + move)
+			{
+				speed = 0;
+			}
+		}
+	}
+	void OnTriggerEnter(Collider c) {
+		if (c.tag == "Player" && Player.isEarth)
+		{	
+			
+			right = true;
+
+		}
+	}
+}
---
+++ gameDev/gameDev/Assets/scripts/triggerStoneWalls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerStoneWalls : MonoBehaviour {
+
+	public float move = 4;
+	public float speed = 10;
+	private float initialPosition ;
+
+	public bool up = false;
+	public bool down = false;
+	private bool moveUp = false;
+	private bool moveDown = false; 
+
+	public GameObject wall;
+	// Use this for initialization
+	void Start () {
+		initialPosition = wall.transform.position.y;
+		Debug.Log(initialPosition + "wall");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(moveUp){
+			wall.transform.Translate(0,speed*Time.deltaTime,0);
+			if(wall.transform.position.y >= initialPosition + move)
+			{
+				speed = 0;
+			}
+		}
+		if(moveDown){
+			wall.transform.Translate(0,-speed*Time.deltaTime,0);
+			if(wall.transform.position.y <= initialPosition - move)
+			{
+				speed = 0;
+			}
+		}		
+	}
+	void OnTriggerEnter(Collider c) {
+		if (c.tag == "FuckYouStone" && up)
+		{
+			moveUp = true;
+		}
+		if (c.tag == "FuckYouStone" && down)
+		{
+			moveDown = true;
+		}
+	}
+}
